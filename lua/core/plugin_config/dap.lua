c7ccfb68624185657131e23ad9aca03cdcba08f1
Sean Tronsen <sean.tronsen@gmail.com>
2023-04-29 09:22:46 -0400

    [repl] -- added run to cursor and step out of current function.

--- lua/core/plugin_config/dap.lua
+++ lua/core/plugin_config/dap.lua
@@ -109,8 +109,7 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
 
--- KEYBINDINGS
-vim.keymap.set("n", "<leader>dx", dapui.toggle, { desc = "[d]ap toggle [b]reakpoint" })
+-- KEYBINDINGS DAP
 vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "[d]ap toggle [b]reakpoint" })
 vim.keymap.set(
 	"n",
@@ -118,11 +117,16 @@ vim.keymap.set(
 	dap.continue,
 	{ desc = "[d]ap [c]ontinue (will start the debugger for certain languages)" }
 )
-vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "[d]ap [t]erminate" })
-vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "[d]ap step [o]ver" })
+vim.keymap.set("n", "<leader>ds", dap.terminate, { desc = "[d]ap [s]top (terminate)" })
+vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "[d]ap step [n]ext (step over)" })
+vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "[d]ap step [o]ut" })
 vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "[d]ap step [i]nto" })
+vim.keymap.set("n", "<leader>dt", dap.run_to_cursor, { desc = "[d]ap run [t]o cursor" })
 vim.keymap.set("n", "<leader>dd", dap.down, { desc = "[d]ap travel [d]own the stack" })
 vim.keymap.set("n", "<leader>du", dap.up, { desc = "[d]ap travel [u]p the stack" })
+
+-- KEYBINDINGS DAPUI
+vim.keymap.set("n", "<leader>dx", dapui.toggle, { desc = "[d]ap toggle UI" })
 vim.keymap.set("n", "<leader>dr", function()
 	dapui.open(dapui_open_args)
 end, { desc = "[d]ap [r]eset ui" })
