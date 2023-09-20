c3ec15a9fa0fa8b5650881da2db8a84844707d6f
Sean Tronsen <sean.tronsen@gmail.com>
2023-09-20 09:46:56 -0600

    [main] -- added a few markdown equation bindings for notes.

---
+++ snippets/markdown.lua
@@ -0,0 +1,57 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+return {
+
+	s(
+		"bme",
+		fmta(
+			[[
+	$$
+	<>
+	$$
+	]],
+			{ i(1, "content") }
+		)
+	),
+	s("bmi", fmta("$<>$", { i(1, "content") })),
+	s(
+		"benv",
+		fmta(
+			[[
+	\begin{<>}
+
+	<>
+
+	\end{<>}
+	]],
+			{ i(1, "section-type"), i(2, "content"), rep(1) }
+		)
+	),
+	s(
+		"beq",
+		fmta(
+			[[
+	\begin{equation}
+
+	<>
+
+	\end{equation}
+	]],
+			{ i(1, "content") }
+		)
+	),
+	s("mf", fmta("\frac {<>}{<>} ", { i(1, "numerator"), i(2, "denominator") })),
+	s("mpro", fmta("\\prod_{<>}^{<>} <>", { i(1, "start"), i(2, "stop"), i(3, "equation") })),
+	s("msum", fmta("\\sum_{<>}^{<>} <>", { i(1, "start"), i(2, "stop"), i(3, "equation") })),
+	s("mint", fmta("\\int_{<>}^{<>} <>", { i(1, "start"), i(2, "stop"), i(3, "equation") })),
+}
