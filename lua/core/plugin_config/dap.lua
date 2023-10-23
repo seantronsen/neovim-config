cd1be23a5bb2eec5ba57a3e4cfda99a6cc7fcc3c
Sean Tronsen <sean.tronsen@gmail.com>
2023-10-23 15:41:30 -0600

    [main] -- pulled in server configuration for bash debugging

--- lua/core/plugin_config/dap.lua
+++ lua/core/plugin_config/dap.lua
@@ -14,8 +14,36 @@ dap.adapters.codelldb = {
 	},
 }
 
+dap.adapters.bashdb = {
+	type = "executable",
+	command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+	name = "bashdb",
+}
+
 -- DEFINE DEBUG CONFIGURATIONS FOR DAP
 ------------------------------------------
+dap.configurations.sh = {
+	{
+		type = "bashdb",
+		request = "launch",
+		name = "Launch file",
+		showDebugOutput = true,
+		pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+		pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+		trace = true,
+		file = "${file}",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+		pathCat = "cat",
+		pathBash = "/bin/bash",
+		pathMkfifo = "mkfifo",
+		pathPkill = "pkill",
+		args = {},
+		env = {},
+		terminalKind = "integrated",
+	},
+}
+
 dap.configurations.c = {
 	{
 		name = "Launch file",
