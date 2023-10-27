efd427ba592c9f8038978d3e782438379fc1b300
Sean Tronsen <sean.tronsen@gmail.com>
2023-10-27 14:59:07 -0600

    [nvim9] -- configuration of lsp saga.

--- lua/core/plugin_config/vimtex.lua
+++ lua/core/plugin_config/vimtex.lua
@@ -1,10 +1,10 @@
 vim.cmd("filetype plugin on")
 vim.cmd("syntax enable")
-local in_mathzone = function()
-	local is_mathzone = vim.fn["vimtex#syntax#in_mathzone"]() == 1
-	print("is math zone: " .. tostring(is_mathzone))
-	return is_mathzone
-end
+-- local in_mathzone = function()
+-- 	local is_mathzone = vim.fn["vimtex#syntax#in_mathzone"]() == 1
+-- 	print("is math zone: " .. tostring(is_mathzone))
+-- 	return is_mathzone
+-- end
 
 local markdown_math_toggle = function()
 	local cft = vim.bo.filetype
@@ -19,9 +19,8 @@ local markdown_math_toggle = function()
 	end
 end
 
-vim.keymap.set("n", "<leader>mz", in_mathzone, { desc = "is [m]ath[z]one" })
+-- vim.keymap.set("n", "<leader>mz", in_mathzone, { desc = "is [m]ath[z]one" })
 vim.keymap.set("n", "<leader>mt", markdown_math_toggle, { desc = "[m]arkdown [m]ath [t]oggle" })
-
 
 vim.g.vimtex_syntax_enabled = 0
 vim.g.vimtex_syntax_conceal_disable = 1
