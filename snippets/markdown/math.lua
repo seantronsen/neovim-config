23bce7e6955fbbdf8d699fd00f3b1088365a400c
Sean Tronsen <sean.tronsen@gmail.com>
2023-11-02 09:54:37 -0600

    [main] -- removed garbage file.

--- snippets/markdown/math.lua
+++
@@ -1,8 +0,0 @@
--- temporary file result from refactoring. keeping for a while until the edits below are finished.
-
--- add snippet for plane notation (P_1) with the weird squiggle P
--- add snippet for stackable primes (derivatives) integrals, etc.
--- snippets should include the \ symbol when consuming for easier stacking.
--- add snippet for parallel
-
-return {}
