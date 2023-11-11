54dd0b669c6afaf43ea7c9dc0228b7b00aa302f1
Sean Tronsen <sean.tronsen@gmail.com>
2023-11-11 11:26:21 -0700

    [main] -- fixed issue with obsidian configuration after update.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,16 +1,3 @@
--- note taking
-vim.keymap.set("n", "<leader>ol", function() vim.cmd("ObsidianFollowLink") end, { desc = "[o]bsidian follow [l]ink" })
-vim.keymap.set("n", "<leader>fn", function() vim.cmd("ObsidianSearch") end, { desc = "[f]ind obsidian [n]ote (search vault)" })
-vim.keymap.set("n", "<leader>ob", function() vim.cmd("ObsidianBacklinks") end, { desc = "[o]bsidian [b]acklinks" })
-vim.keymap.set("n", "<leader>on", function()
-	local result = vim.fn.input("Note name: ")
-	if vim.fn.empty(result) == 1 then
-		print("error: name cannot be empty")
-	else
-		vim.cmd("ObsidianNew " .. result)
-	end
-end, { desc = "[o]bsidian create [n]ew note" })
-
 -- formatting
 vim.keymap.set("n", "<leader>re", function() vim.cmd("Format") end, { desc = "[r]e[f]ormat" })
 vim.keymap.set("n", "<leader>nh", function() vim.cmd("nohls") end, { desc = "[n]o [h]ighlight" })
--- lua/core/plugin_config/obsidian.lua
+++ lua/core/plugin_config/obsidian.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 local obs = require("obsidian")
 
 obs.setup({
@@ -17,9 +18,11 @@ obs.setup({
 	-- },
 
 	mappings = {
-		["<leader>gl"] = function()
-			vim.cmd("ObsidianFollowLink")
-		end,
+		["<leader>gl"] = {
+			action = function()
+				vim.cmd("ObsidianFollowLink")
+			end,
+		},
 	},
 	note_id_func = function(title)
 		-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
@@ -35,7 +38,29 @@ obs.setup({
 				suffix = suffix .. string.char(math.random(65, 90))
 			end
 		end
-		return tostring(os.time()) .. "-" .. suffix
+		return suffix .. "-" .. tostring(os.time())
 	end,
 	finder = "telescope.nvim",
 })
+
+-- note taking
+vim.keymap.set("n", "<leader>ol", function()
+	vim.cmd("ObsidianFollowLink")
+end, { desc = "[o]bsidian follow [l]ink" })
+
+vim.keymap.set("n", "<leader>fn", function()
+	vim.cmd("ObsidianSearch")
+end, { desc = "[f]ind obsidian [n]ote (search vault)" })
+
+vim.keymap.set("n", "<leader>ob", function()
+	vim.cmd("ObsidianBacklinks")
+end, { desc = "[o]bsidian [b]acklinks" })
+
+vim.keymap.set("n", "<leader>on", function()
+	local result = vim.fn.input("Note name: ")
+	if vim.fn.empty(result) == 1 then
+		print("error: name cannot be empty")
+	else
+		vim.cmd("ObsidianNew " .. result)
+	end
+end, { desc = "[o]bsidian create [n]ew note" })
