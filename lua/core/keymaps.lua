97511d01775d05cc8f6ccf5c2fdb697eccd38f92
Sean Tronsen <sean.tronsen@gmail.com>
2023-11-17 14:45:31 -0700

    [main] -- new keymaps for vimtex.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,6 @@
 -- formatting
 vim.keymap.set("n", "<leader>re", function() vim.cmd("Format") end, { desc = "[r]e[f]ormat" })
 vim.keymap.set("n", "<leader>nh", function() vim.cmd("nohls") end, { desc = "[n]o [h]ighlight" })
+
+vim.keymap.set("n", "<leader>vv", function() vim.cmd("VimtexView") end, { desc = "[v]imtex [v]iew" })
+vim.keymap.set("n", "<leader>vc", function() vim.cmd("VimtexView") end, { desc = "[v]imtex [c]ompile (sync)" })
