94ef6329d85f93186c496a01e518e4bfbbf05a8c
Sean Tronsen <sean.tronsen@gmail.com>
2023-11-21 12:26:23 -0700

    [main] -- updated search to include hidden files.

--- lua/core/plugin_config/search.lua
+++ lua/core/plugin_config/search.lua
@@ -16,13 +16,32 @@ require("telescope").setup({
 	},
 })
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
+
+vim.keymap.set("n", "<leader>ff", function()
+	builtin.find_files({ hidden = true })
+end, { desc = "[f]ind [f]iles" })
+
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind with live [g]rep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]uffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp tags" })
-vim.keymap.set("n", "<leader>fo", builtin.lsp_outgoing_calls, { desc = "[f]ind lsp [o]utgoing funcion calls" })
-vim.keymap.set("n", "<leader>fi", builtin.lsp_incoming_calls, { desc = "[f]ind lsp [i]ncoming funcion calls" })
+vim.keymap.set(
+	"n",
+	"<leader>fo",
+	builtin.lsp_outgoing_calls,
+	{ desc = "[f]ind lsp [o]utgoing funcion calls" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fi",
+	builtin.lsp_incoming_calls,
+	{ desc = "[f]ind lsp [i]ncoming funcion calls" }
+)
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[f]ind [k]eymaps" })
 vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "[f]ind and execute [c]ommands" })
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[f]ind [d]iagnostics (code issues)" })
+vim.keymap.set(
+	"n",
+	"<leader>fd",
+	builtin.diagnostics,
+	{ desc = "[f]ind [d]iagnostics (code issues)" }
+)
 vim.keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "[f]ind [m]an pages" })
