332dbb2fdd69224905947624a8c579f75eb850a8
Sean Tronsen <sean.tronsen@gmail.com>
2024-05-29 08:59:59 -0600

    [main] -- updated implementation notes for img-clip on macOS.

--- lua/core/plugin_config/clipboard.lua
+++ lua/core/plugin_config/clipboard.lua
@@ -1,11 +1,14 @@
 -- enable users to persist an image on the clipboard to the filesystem and
 -- simultaneously paste in a reference to that image, formatted appropriately for the markup language type.
 
+-- important: remember to install pngpaste when editting on macOS
+-- https://github.com/jcsalterego/pngpaste
 local image_clipper = require("img-clip")
 image_clipper.setup({
 	default = {
 		dir_path = "attachments",
-		file_name = "%Y%m%d%H%M%S",
+		file_name = "img-clip-%Y%m%d%H%M%S",
+		-- extension = "jpg",
 		relative_to_current_file = true,
 		show_dir_path_in_prompt = true,
 	},
