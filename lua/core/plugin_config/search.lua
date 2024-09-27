57acffb6968c554af7e88bac87326c2cba389d35
Sean Tronsen <sean.tronsen@gmail.com>
2024-09-26 20:27:21 -0600

    [main] -- added path focused searching.

--- lua/core/plugin_config/search.lua
+++ lua/core/plugin_config/search.lua
@@ -48,3 +48,32 @@ vim.keymap.set(
 	{ desc = "[f]ind [d]iagnostics (code issues)" }
 )
 vim.keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "[f]ind [m]an pages" })
+
+-- new bindings to search relative to the current buffer position.
+-- created because I got tired of having to exit to have an easier time searching plugin package directories.
+local path_picker_builder = function(builtin_func)
+	return function()
+		local target_dir
+		if vim.bo.filetype == "oil" then
+			target_dir = require("oil").get_current_dir()
+		else
+			target_dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+		end
+		print(target_dir)
+		builtin_func({ cwd = target_dir })
+	end
+end
+
+vim.keymap.set(
+	"n",
+	"<leader>fpf",
+	path_picker_builder(builtin.find_files),
+	{ desc = "[f]ind [f]iles (relative to [p]ath)" }
+)
+
+vim.keymap.set(
+	"n",
+	"<leader>fpg",
+	path_picker_builder(builtin.live_grep),
+	{ desc = "[f]ind with live [g]rep (relative to [p]ath)" }
+)
