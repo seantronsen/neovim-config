2de73e723ca14c23e047ae31248cb0b7e0810f72
Sean Tronsen <sean.tronsen@gmail.com>
2024-10-01 16:58:21 -0600

    [main] -- fixed <C-w> pain point issue inside dap (prompt type) buffers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,3 +4,23 @@ vim.keymap.set("n", "<leader>nh", function() vim.cmd("nohls") end, { desc = "[n]
 
 vim.keymap.set("n", "<leader>vv", function() vim.cmd("VimtexView") end, { desc = "[v]imtex [v]iew" })
 vim.keymap.set("n", "<leader>vc", function() vim.cmd("VimtexCompile") end, { desc = "[v]imtex [c]ompile (sync)" })
+
+-- note: fixes pain point issue where <C-w> keybinding doesn't substitute-style
+-- delete the word before the cursor while in insert mode inside a `prompt`
+-- type buffer. according to the nvim docs here (accessed 10/01/2024):
+-- <https://neovim.io/doc/user/channel.html#_5.-using-a-prompt-buffer>, the
+-- maintainers have designed this as the default behavior within nvim. however,
+-- it's not for me as I prefer consistency. 
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	callback = function()
+		if vim.bo.buftype == "prompt" then
+			vim.keymap.set(
+				"i",
+				"<C-w>",
+				"<S-C-w>",
+				{ noremap = true, silent = true, buffer = true }
+			)
+		end
+	end,
+})
