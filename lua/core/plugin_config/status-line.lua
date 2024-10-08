b396616468ce1924d5709860477ff16e62488237
Sean Tronsen <sean.tronsen@gmail.com>
2024-10-08 16:04:48 -0600

    [main] -- changed lualine to use ascii chars.

--- lua/core/plugin_config/status-line.lua
+++ lua/core/plugin_config/status-line.lua
@@ -1,7 +1,9 @@
 require("lualine").setup({
 	options = {
-		icons_enabled = true,
-		theme = "carbonfox",
+		icons_enabled = false,
+		theme = "auto",
+		component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
 	},
 	sections = {
 		lualine_c = {
