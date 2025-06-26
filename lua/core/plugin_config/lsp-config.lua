2badc8df78d8f0daa48aa40d5651fdd93064454e
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-26 07:37:58 -0600

    temporary stopping point for lsp updates

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -24,61 +24,58 @@ vim.keymap.set(
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 
-local on_attach = function(client, bufnr)
-	-- disable formatting from non formatter.nvim sources, so it doesn't
-	-- interfere with gqq
-	vim.bo[bufnr].formatexpr = nil
-	-- Mappings.
-	-- See  for documentation on any of the below functions
-	-- local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "[g]o to [D]eclaration" })
-	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" })
-	vim.keymap.set(
-		"n",
-		"<leader>gt",
-		vim.lsp.buf.type_definition,
-		{ desc = "[g]o to symbol [t]ype definition" }
-	)
-	vim.keymap.set(
-		"n",
-		"<leader>gi",
-		vim.lsp.buf.implementation,
-		{ desc = "[g]o to [i]mplementation" }
-	)
-	vim.keymap.set(
-		"n",
-		"<leader>h",
-		vim.lsp.buf.hover,
-		{ desc = "[h]elp (information for hovered item)" }
-	)
-	vim.keymap.set(
-		"n",
-		"<leader>fr",
-		require("telescope.builtin").lsp_references,
-		{ desc = "[f]ind [r]eferences" }
-	)
+vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-	-- CODE ACTION
-	vim.keymap.set("n", "<leader>a", function()
-		vim.cmd([[Lspsaga code_action]])
-	end, { desc = "code [a]ction" })
+			-- disable formatting from non formatter.nvim sources, so it doesn't
+				-- interfere with gqq
+				vim.bo[bufnr].formatexpr = nil
+				-- Mappings.
+				-- See  for documentation on any of the below functions
+				-- local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "[g]o to [D]eclaration" })
+				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" })
+				vim.keymap.set(
+					"n",
+					"<leader>gt",
+					vim.lsp.buf.type_definition,
+					{ desc = "[g]o to symbol [t]ype definition" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>gi",
+					vim.lsp.buf.implementation,
+					{ desc = "[g]o to [i]mplementation" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>h",
+					vim.lsp.buf.hover,
+					{ desc = "[h]elp (information for hovered item)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>fr",
+					require("telescope.builtin").lsp_references,
+					{ desc = "[f]ind [r]eferences" }
+				)
 
-	-- REFACTORING
-	vim.keymap.set("n", "<leader>rr", function()
-		vim.cmd([[Lspsaga rename]])
-	end, { desc = "[r]efactor [r]ename symbol and references" })
+				-- CODE ACTION
+				vim.keymap.set("n", "<leader>a", function()
+					vim.cmd([[Lspsaga code_action]])
+				end, { desc = "code [a]ction" })
 
-	vim.keymap.set("n", "<leader>rp", function()
-		local name_old = vim.fn.input("old name: ")
-		local name_new = vim.fn.input("new name: ")
+				-- REFACTORING
+				vim.keymap.set("n", "<leader>rr", function()
+					vim.cmd([[Lspsaga rename]])
+				end, { desc = "[r]efactor [r]ename symbol and references" })
 
-		if vim.fn.empty(name_old) == 1 or vim.fn.empty(name_new) then
-			print("error: name cannot be empty")
-		else
-			vim.cmd([[Lspsaga project_replace ]] .. name_old .. " " .. name_new)
-		end
-	end, { desc = "[r]efactor rename project matches" })
-end
+
+      end
+})
+
+
 
 local lsp_flags = { debounce_text_changes = 100 }
 
@@ -111,78 +108,82 @@ require("mason-tool-installer").setup({
 require("neodev").setup()
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local lspsaga = require("lspsaga")
-lspsaga.setup({
+-- possible deletion candidate
+--
+-- local lspsaga = require("lspsaga")
+-- lspsaga.setup({
+-- 
+-- 	ui = {
+-- 		code_action = "A",
+-- 	},
+-- 	lightbulb = {
+-- 		sign = false,
+-- 	},
+-- 	implement = {
+-- 		sign = false,
+-- 	},
+-- })
 
-	ui = {
-		code_action = "A",
-	},
-	lightbulb = {
-		sign = false,
-	},
-	implement = {
-		sign = false,
-	},
-})
+-- possible deletion candidate
+-- 
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "bats",
+-- 	callback = function()
+-- 		vim.cmd([[set ft=bash]])
+-- 	end,
+-- })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "bats",
-	callback = function()
-		vim.cmd([[set ft=bash]])
-	end,
-})
-
-require("mason-lspconfig").setup_handlers({
-	--------------------------------
-	-- the default handler that's called for any installed server that
-	-- doesn't already have a dedicated handler.
-	--
-	-- note that it makes some assumptions about the nature of
-	-- the server (compare the default with the setup for
-	-- rust_analyzer).
-	--------------------------------
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-			flags = lsp_flags,
-			capabilities = capabilities,
-		})
-	end,
-
-	["lua_ls"] = function()
-		require("lspconfig").lua_ls.setup({
-			on_attach = on_attach,
-			flags = lsp_flags,
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					-- diagnostics = {
-					-- 	globals = { "vim" },
-					-- },
-					workspace = {
-						checkThirdParty = false,
-					},
-				},
-			},
-		})
-	end,
-
-	-- todo: possibly unnecessary
-	["pyright"] = function()
-		require("lspconfig").pyright.setup({
-			on_attach = on_attach,
-			flags = lsp_flags,
-			capabilities = capabilities,
-			settings = {
-				python = {
-					analysis = {
-						diagnosticMode = "workspace",
-						useLibraryCodeForTypes = true,
-						typeCheckingMode = "strict",
-						stubPath = "typings",
-					},
-				},
-			},
-		})
-	end,
-})
+-- require("mason-lspconfig").setup_handlers({
+-- 	--------------------------------
+-- 	-- the default handler that's called for any installed server that
+-- 	-- doesn't already have a dedicated handler.
+-- 	--
+-- 	-- note that it makes some assumptions about the nature of
+-- 	-- the server (compare the default with the setup for
+-- 	-- rust_analyzer).
+-- 	--------------------------------
+-- 	function(server_name)
+-- 		require("lspconfig")[server_name].setup({
+-- 			on_attach = on_attach,
+-- 			flags = lsp_flags,
+-- 			capabilities = capabilities,
+-- 		})
+-- 	end,
+-- 
+-- 	["lua_ls"] = function()
+-- 		require("lspconfig").lua_ls.setup({
+-- 			on_attach = on_attach,
+-- 			flags = lsp_flags,
+-- 			capabilities = capabilities,
+-- 			settings = {
+-- 				Lua = {
+-- 					-- diagnostics = {
+-- 					-- 	globals = { "vim" },
+-- 					-- },
+-- 					workspace = {
+-- 						checkThirdParty = false,
+-- 					},
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- 
+-- 	-- todo: possibly unnecessary
+-- 	["pyright"] = function()
+-- 		require("lspconfig").pyright.setup({
+-- 			on_attach = on_attach,
+-- 			flags = lsp_flags,
+-- 			capabilities = capabilities,
+-- 			settings = {
+-- 				python = {
+-- 					analysis = {
+-- 						diagnosticMode = "workspace",
+-- 						useLibraryCodeForTypes = true,
+-- 						typeCheckingMode = "strict",
+-- 						stubPath = "typings",
+-- 					},
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- })
