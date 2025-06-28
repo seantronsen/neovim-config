fa001b76be6410b26300d8325f280d8dd4cfd384
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 15:26:16 -0600

    add note about deprecated launch.json config

--- lua/core/plugin_config/dap/base.lua
+++ lua/core/plugin_config/dap/base.lua
@@ -12,6 +12,11 @@ local launch_filetype_maps = {
 	debugpy = { "python" },
 	codelldb = { "c", "cpp" },
 }
+
+-- this method of config is deprecated as of Sat Jun 28 03:24:11 PM MDT 2025
+-- however, the new batteries included method seems to mandate placing the
+-- config in a `.vscode/.launch.json` file, which I'm not willing to conform
+-- to.
 require("dap.ext.vscode").load_launchjs(launch_path, launch_filetype_maps)
 
 -- SETUP VIRTUAL TEXT
