1e73d6cc9969ad9054484a923a37e9242fb19539
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 12:10:57 -0600

    simplify syntax highlights

--- lua/core/plugin_config/syntax-highlight.lua
+++ lua/core/plugin_config/syntax-highlight.lua
@@ -1,25 +1,32 @@
 -- CONFIGURATION FOR SYNTAX HIGHLIGHTING
 --------------------------------
--- fix the tree sitter install and runtime path directories
-
-local is_headless = require("core.utils").is_running_in_headless_mode()
-
 local path_parser = vim.fn.stdpath("data") .. "/parsers"
 vim.opt.runtimepath:append(path_parser)
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = is_headless and {}
-		or require("core.plugin_config.treesitter.treesitter-parsers"),
-	sync_install = is_headless,
+	parser_install_dir = path_parser,
+	ensure_installed = {
+		"bash",
+		"c",
+		"cpp",
+		"dockerfile",
+		"go",
+		"html",
+		"json",
+		"jsonc",
+		"lua",
+		"make",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"rust",
+		"toml",
+		"vim",
+		"yaml",
+	},
 	auto_install = false,
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = { "markdown", "latex" },
 	},
-
-	-- disable = {"latex", "markdown"},
-	--ignore_install = {"latex", "markdown"},
-
-	-- todo: this should use the nvim vars like mason does
-	parser_install_dir = path_parser,
 })
