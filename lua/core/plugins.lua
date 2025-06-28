58c864d317045c5e8646c46588ba1da08b144d7f
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 13:00:10 -0600

    add setup for lsp tools during editor configuration

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -75,6 +75,13 @@ require("lazy").setup({
 	--------------------------------
 	{
 		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
 		dependencies = {
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
@@ -89,4 +96,18 @@ require("lazy").setup({
 	-- AUTODOCUMENTATION
 	--------------------------------
 	{ "danymat/neogen", tag = "2.17.1" },
+
+	-- CONFIGURATION TOOLS
+	--------------------------------
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }, { rocks = { enabled = false } })
