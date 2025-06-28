2e89a65952d53a06049be42d21618c76b34a1249
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 13:20:33 -0600

    clean up keymaps and fix some bad desc keys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,21 +1,19 @@
 -- ease of use -- sanity -- pane motion without the tmux bullshit
-
-vim.keymap.set({"n", "i"}, "<C-h>", function () vim.cmd("wincmd h") end, {desc="focus pane up"})
-vim.keymap.set({"n", "i"}, "<C-j>", function () vim.cmd("wincmd j") end, {desc="focus pane up"})
+vim.keymap.set({"n", "i"}, "<C-h>", function () vim.cmd("wincmd h") end, {desc="focus pane left"})
+vim.keymap.set({"n", "i"}, "<C-j>", function () vim.cmd("wincmd j") end, {desc="focus pane down"})
 vim.keymap.set({"n", "i"}, "<C-k>", function () vim.cmd("wincmd k") end, {desc="focus pane up"})
-vim.keymap.set({"n", "i"}, "<C-l>", function () vim.cmd("wincmd l") end, {desc="focus pane up"})
-
+vim.keymap.set({"n", "i"}, "<C-l>", function () vim.cmd("wincmd l") end, {desc="focus pane right"})
 
 -- formatting
 vim.keymap.set("n", "<leader>re", function() vim.cmd("Format") end, { desc = "[r]e[f]ormat" })
 vim.keymap.set("n", "<leader>nh", function() vim.cmd("nohls") end, { desc = "[n]o [h]ighlight" })
 
--- note: fixes pain point issue where <C-w> keybinding doesn't substitute-style
+-- NOTE: fixes pain point issue where <C-w> keybinding doesn't substitute-style
 -- delete the word before the cursor while in insert mode inside a `prompt`
 -- type buffer. according to the nvim docs here (accessed 10/01/2024):
 -- <https://neovim.io/doc/user/channel.html#_5.-using-a-prompt-buffer>, the
 -- maintainers have designed this as the default behavior within nvim. however,
--- it's not for me as I prefer consistency. 
+-- I prefer consistency...
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*",
 	callback = function()
