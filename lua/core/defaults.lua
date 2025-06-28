d3274ffcdaa31d68d25e34fd6233ffbcbfc41470
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 13:02:18 -0600

    remove default settings for plugins that no longer exist

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -24,26 +24,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_julia_provider = 0
 
 ------------------------
---  VIM-SLIME
-------------------------
--- vim.g.slime_target = "neovim"
--- vim.g.slime_no_mappings = 1
--- vim.g.slime_python_ipython = 1
--- vim.g.slime_cell_delimiter = "^\\s*# %%"
--- vim.g.slime_cells_no_highlight = 1
-
-------------------------
 --  VIMTEX
 ------------------------
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_quickfix_open_on_warning = 0
-
-------------------------
---  VIM-DOGE
-------------------------
-vim.g.doge_enable_mappings = 0
-vim.g.doge_doc_standard_python = "reST"
-vim.g.doge_doc_standard_rust = "rustdoc"
-vim.g.doge_doc_standard_sh = "google"
-vim.g.doge_doc_standard_lua = "ldoc"
-vim.g.doge_install_path = vim.fn.stdpath("data") .. "/autodoc/doge"
