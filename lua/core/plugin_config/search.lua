09e33e29e27a13c9a314e54fec6191e3be14fa4d
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 14:18:53 -0600

    clean up telescope config

--- lua/core/plugin_config/search.lua
+++ lua/core/plugin_config/search.lua
@@ -25,38 +25,19 @@ require("telescope").setup({
 })
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", function()
-	builtin.find_files()
-	-- builtin.find_files({ hidden = true })
-end, { desc = "[f]ind [f]iles" })
-
+-- builtin.find_files({ hidden = true })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind with live [g]rep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]uffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp tags" })
-vim.keymap.set(
-	"n",
-	"<leader>fo",
-	builtin.lsp_outgoing_calls,
-	{ desc = "[f]ind lsp [o]utgoing funcion calls" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fi",
-	builtin.lsp_incoming_calls,
-	{ desc = "[f]ind lsp [i]ncoming funcion calls" }
-)
+vim.keymap.set("n", "<leader>fo", builtin.lsp_outgoing_calls, { desc = "[f]ind lsp [o]utgoing funcion calls" })
+vim.keymap.set("n", "<leader>fi", builtin.lsp_incoming_calls, { desc = "[f]ind lsp [i]ncoming funcion calls" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[f]ind [k]eymaps" })
 vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "[f]ind and execute [c]ommands" })
-vim.keymap.set(
-	"n",
-	"<leader>fd",
-	builtin.diagnostics,
-	{ desc = "[f]ind [d]iagnostics (code issues)" }
-)
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[f]ind [d]iagnostics (code issues)" })
 vim.keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "[f]ind [m]an pages" })
 
--- new bindings to search relative to the current buffer position.
--- created because I got tired of having to exit to have an easier time searching plugin package directories.
+-- bindings to search relative to the current buffer position
 local path_picker_builder = function(builtin_func)
 	return function()
 		local target_dir
@@ -70,16 +51,5 @@ local path_picker_builder = function(builtin_func)
 	end
 end
 
-vim.keymap.set(
-	"n",
-	"<leader>fpf",
-	path_picker_builder(builtin.find_files),
-	{ desc = "[f]ind [f]iles (relative to [p]ath)" }
-)
-
-vim.keymap.set(
-	"n",
-	"<leader>fpg",
-	path_picker_builder(builtin.live_grep),
-	{ desc = "[f]ind with live [g]rep (relative to [p]ath)" }
-)
+vim.keymap.set("n", "<leader>fpf", path_picker_builder(builtin.find_files), { desc = "find files (relative)" })
+vim.keymap.set("n", "<leader>fpg", path_picker_builder(builtin.live_grep), { desc = "find with live grep (relative)" })
