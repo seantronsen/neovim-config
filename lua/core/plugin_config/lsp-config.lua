9419845d6fd1322b7dac6d3cf078eaaf906ef440
Sean Tronsen <sean.tronsen@gmail.com>
2025-06-28 11:44:50 -0600

    migrate lsp behavior configuration to 0.11 convention

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -112,66 +112,61 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- 	},
 -- })
 
--- possible deletion candidate
--- 
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "bats",
--- 	callback = function()
--- 		vim.cmd([[set ft=bash]])
--- 	end,
--- })
+vim.lsp.config("pyright", {
+	settings = {
+		python = {
+			analysis = {
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "strict",
+				stubPath = "typings",
+			},
+		},
+	},
+})
 
--- require("mason-lspconfig").setup_handlers({
--- 	--------------------------------
--- 	-- the default handler that's called for any installed server that
--- 	-- doesn't already have a dedicated handler.
--- 	--
--- 	-- note that it makes some assumptions about the nature of
--- 	-- the server (compare the default with the setup for
--- 	-- rust_analyzer).
--- 	--------------------------------
--- 	function(server_name)
--- 		require("lspconfig")[server_name].setup({
--- 			on_attach = on_attach,
--- 			flags = lsp_flags,
--- 			capabilities = capabilities,
--- 		})
--- 	end,
--- 
--- 	["lua_ls"] = function()
--- 		require("lspconfig").lua_ls.setup({
--- 			on_attach = on_attach,
--- 			flags = lsp_flags,
--- 			capabilities = capabilities,
--- 			settings = {
--- 				Lua = {
--- 					-- diagnostics = {
--- 					-- 	globals = { "vim" },
--- 					-- },
--- 					workspace = {
--- 						checkThirdParty = false,
--- 					},
--- 				},
--- 			},
--- 		})
--- 	end,
--- 
--- 	-- todo: possibly unnecessary
--- 	["pyright"] = function()
--- 		require("lspconfig").pyright.setup({
--- 			on_attach = on_attach,
--- 			flags = lsp_flags,
--- 			capabilities = capabilities,
--- 			settings = {
--- 				python = {
--- 					analysis = {
--- 						diagnosticMode = "workspace",
--- 						useLibraryCodeForTypes = true,
--- 						typeCheckingMode = "strict",
--- 						stubPath = "typings",
--- 					},
--- 				},
--- 			},
--- 		})
--- 	end,
--- })
+-- pulled from: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+vim.lsp.config("lua_ls", {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+				path ~= vim.fn.stdpath("config")
+				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+			then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most
+				-- likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Tell the language server how to find Lua modules same way as Neovim
+				-- (see `:h lua-module-load`)
+				path = {
+					"lua/?.lua",
+					"lua/?/init.lua",
+				},
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+			},
+		})
+	end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+		},
+	},
+})
