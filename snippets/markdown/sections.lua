a129b3fe08159274f5a9520b86223cf5c21ac800
Sean Tronsen <sean.tronsen@gmail.com>
2025-09-02 07:09:08 -0600

    var rename

--- snippets/markdown/sections.lua
+++ snippets/markdown/sections.lua
@@ -10,48 +10,48 @@ local fmta = require("luasnip.extras.fmt").fmta
 local fmt = require("luasnip.extras.fmt").fmt
 
 local snutils = require("core.snippetutils")
-local obsidian_header = [[> [!{}] {}
+local markdown_header = [[> [!{}] {}
 >
 > ]]
 
 return {}, {
 	s(
 		{ trig = "^sthe", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("important"), ntext("Theorem") }),
+		fmt(markdown_header, { ntext("important"), ntext("Theorem") }),
 		{ condition = snutils.not_mathzone }
 	),
 	s(
 		{ trig = "^sdef", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("important"), ntext("Definition") }),
+		fmt(markdown_header, { ntext("important"), ntext("Definition") }),
 		{ condition = snutils.not_mathzone }
 	),
 	s(
 		{ trig = "^snot", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("info"), ntext("Note") }),
+		fmt(markdown_header, { ntext("info"), ntext("Note") }),
 		{ condition = snutils.not_mathzone }
 	),
 
 	s(
 		{ trig = "^swar", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("warning"), ntext("") }),
+		fmt(markdown_header, { ntext("warning"), ntext("") }),
 		{ condition = snutils.not_mathzone }
 	),
 
 	s(
 		{ trig = "^s!", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("important"), ntext("") }),
+		fmt(markdown_header, { ntext("important"), ntext("") }),
 		{ condition = snutils.not_mathzone }
 	),
 
 	s(
 		{ trig = "^s%?", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("faq"), ntext("Common Questions") }),
+		fmt(markdown_header, { ntext("faq"), ntext("Common Questions") }),
 		{ condition = snutils.not_mathzone }
 	),
 
 	s(
 		{ trig = "^serr", wordTrig = false, regTrig = true },
-		fmt(obsidian_header, { ntext("fail"), ntext("") }),
+		fmt(markdown_header, { ntext("fail"), ntext("") }),
 		{ condition = snutils.not_mathzone }
 	),
 }
